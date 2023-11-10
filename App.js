8e9dd3be26c7d6bccee689c246a1da671b0f0085
Harsh Mishra <mishra_delhi1@ymail.com>
2023-11-11 00:28:38 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,20 @@
+// const naame = React.createElement("p",{},"MY NAME IS HARSH !");
+//         const root = ReactDOM.createRoot(document.getElementById("root"));
+//         root.render(naame);
+
+const parent = React.createElement("div",
+{id:"parent"},[
+React.createElement("div",{id:"child"},
+[
+        React.createElement("h1",{},"hi"),
+        React.createElement("h2",{},"hello")
+]),
+React.createElement("div",{id:"child2"},[
+ React.createElement("h1",{},"hi am second child"),
+ React.createElement("h1",{},"hi iam harsh misgra")
+])
+]
+);
+console.log(parent);
+const root= ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
---
+++ callback.js
@@ -0,0 +1,30 @@
+// 1. regidter 
+// 2. send welcome email 
+// 3. Login 
+// 4. Get user data 
+// 5. display user data 
+
+function register(){
+console.log('Register ENd')
+
+}
+function sendWelcomeEmail(){
+    console.log('Email sent ')
+}
+function login(){
+    console.log('login done')
+}
+function getUser(){
+    console.log('got user data')
+}
+function displayUserData(){
+    console.log('user data display')
+}
+
+register(); 
+sendWelcomeEmail();
+login();
+getUser(); 
+displayUserData(); 
+
+console.log('Other application work');
