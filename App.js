a8ce1de9f87d987b162891dc465d4248e65db1a8
Harsh Mishra <mishra_delhi1@ymail.com>
2023-11-11 15:59:57 +0530

    Episode 2

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
 // const naame = React.createElement("p",{},"MY NAME IS HARSH !");
 //         const root = ReactDOM.createRoot(document.getElementById("root"));
 //         root.render(naame);
 
-const parent = React.createElement("div",
-{id:"parent"},[
-React.createElement("div",{id:"child"},
+
+
+const parent = React.createElement("div",{id:"parent"},
 [
+React.createElement("div",{id:"child"},
+[      
+        React.createElement("h1",{},"i am harsh"),
         React.createElement("h1",{},"hi"),
         React.createElement("h2",{},"hello")
 ]),
 React.createElement("div",{id:"child2"},[
  React.createElement("h1",{},"hi am second child"),
- React.createElement("h1",{},"hi iam harsh misgra")
+ React.createElement("h1",{},"hi iam harsh misgra ")
 ])
 ]
 );
