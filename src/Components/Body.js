509fd95d6d886c36ff33d713d8442e2b5521b166
Harsh Mishra <mishra_delhi1@ymail.com>
2023-12-09 11:52:54 +0530

    router

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,40 +1,67 @@
 import ResCard from "./ResCard";
-import {resObj} from "../data/resObj";
-import { useState } from "react";
-const Body=()=>{
+import { resObj } from "../data/resObj";
+import { useState ,useEffect} from "react";
 
-        const [res,setRes]=useState(resObj)
-        console.log(res)
-        console.log(setRes)
-    return(
-            <div className="body">
-                    <div className="search-container">
-                    
-                            <div className="filter">
+const Body = () => {
+  const [res, setRes] = useState([]);
 
-                 <button className="filter-btn"
-                         onClick={()=>{
-                              const x= res.filter(
-                                (i)=>i.avgRating>=4
-                                );
-                                console.log(x)
-                                
-                              setRes(x);
-                            }}
-                            >
-                                Top Rated Restaurants
-                            </button>
-                            </div>
-                            
-                            <div className="rest-container">
-                                    {resObj.map((rest)=>(
-                                            <ResCard resData={rest} key={rest.id}/> 
-                                    ))}
 
-                                    
-                            </div>
-                    </div>
-            </div>
+  // const handleFilterClick = () => {
+  //   const filteredData = res.filter((i) => i.avgRating >= 4);
+  //   setRes(filteredData);
+  // };
 
+useEffect(()=>{
+  fetchData();
+},[])
+const fetchData = async ()=>{
+  const data = await fetch(
+    "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.557269&lng=88.302513&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
+  
+const json = await data.json();
+setRes( json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+console.log(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+
+
 };
+// useEffect(()=>{
+// //   fetchData();
+// // },[])
+
+// // const fetchData= async ()=>{
+// //   const data = await fetch(
+// //         "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.848717&lng=77.6727463&collection=83649&tags=layout_CCS_Biryani&sortBy=&filters=&type=rcv2&offset=0&page_type=null"
+// //        );
+      
+// //      const json = await data.json();
+// //      console.log(json.data.cards)
+// //      setRes(json.data.cards[3])
+// // }
+
+
+if(res.length===0){return <h2>Loading....</h2>}
+  return (
+    <div className="body">
+      <div className="search-container">
+        <div className="filter">
+          <button className="filter-btn" onClick={()=>{
+    const x = res.filter((i) => i.avgRating >= 4);
+    setRes(x);
+  }}
+   >
+            Top Rated Restaurants
+          </button>
+        </div>
+
+        <div className="rest-container">
+          {res.map((rest) => (
+            <ResCard resData={rest} key={rest.id} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Body;
--- src/Components/ResCard.js
+++ src/Components/ResCard.js
@@ -9,9 +9,9 @@ const ResCard=(props)=>{
             cuisines,
             costForTwo,
             avgRating,
-            deliveryTime,
+            sla,
             cloudinaryImageId
-    }=resData
+    }=resData?.info
 
     return(
             <div className="Res-Card">
@@ -19,10 +19,10 @@ const ResCard=(props)=>{
                     {/* `${IMG_URL}${cloudinaryImageId}` */}
                             <img className="card-img" src={IMG_URL+cloudinaryImageId} alt="Meghana"/>
                             <h3>{name}</h3>
-                            <h4>{cuisines.join(", ")}</h4>
-                            <h4>Cost for Two:₹{costForTwo/100}</h4>
+                            <h4>{cuisines}</h4>
+                            <h4>{costForTwo}</h4>
                             <h4>{avgRating}</h4>
-                            <h4>{deliveryTime} mins</h4>
+                            <h4>{sla?.deliveryTime} mins</h4>
                     
             </div>
     );
