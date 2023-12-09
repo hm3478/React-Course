149a69a781297283d873903d53ed6cacb76f57aa
Harsh Mishra <mishra_delhi1@ymail.com>
2023-12-09 13:44:40 +0530

    routing error solved

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
 import React from "react";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import ReactDOM from "react-dom/client";
+import  ReactDOM  from "react-dom/client";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Body from "./Components/Body";
+import Conatct from "./Components/Contact";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import About from "./Components/About";
+import Contact from "./Components/Contact";
 
-const AppLayout = () => {
-  return (
-    <div className="app">
-      <Header />
-      <Body />
-      <Footer />
-    </div>
-  );
+const AppLayout =()=>{
+        return(
+                <div className="app">
+                <Header/>
+                <Body/>
+                <Footer/>
+                </div>
+        );
 };
 const appRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: <AppLayout />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider route={appRouter} />);
+           {
+             path: "/", 
+             element: <AppLayout />, 
+            },
+            {
+              path: "/about",
+              element: <About />,
+            },
+            {
+              path: "/contact",
+              element: <Contact/>,
+            },
+
+      ]);
+      const root = ReactDOM.createRoot(document.getElementById("root"));
+      root.render(<RouterProvider router={appRouter} />); // render RouterProvider and use router as props and pass value appRouter
